9d7e8f2b72b3d4cddd6f092017a0f0802e1e06ea
lesly houndole <lesly.houndole@crem.ch>
2019-07-11 12:57:56 +0200

    test

--- cm/app/constant.py
+++ cm/app/constant.py
@@ -24,7 +24,7 @@ INPUTS_CALCULATION_MODULE = [
      'input_type': 'input',
      'input_parameter_name': 'res_heating_share',
      'input_value': 0.33,
-     'input_unit': 'none',
+     'input_unit': '',
      'input_min': 0,
      'input_max': 1,
      'cm_id': CM_ID
@@ -33,7 +33,7 @@ INPUTS_CALCULATION_MODULE = [
      'input_type': 'input',
      'input_parameter_name': 'industry_share',
      'input_value': 0.33,
-     'input_unit': 'none',
+     'input_unit': '',
      'input_min': 0,
      'input_max': 1,
      'cm_id': CM_ID
@@ -42,65 +42,20 @@ INPUTS_CALCULATION_MODULE = [
      'input_type': 'input',
      'input_parameter_name': 'tertiary_share',
      'input_value': 0.33,
-     'input_unit': 'none',
+     'input_unit': '',
      'input_min': 0,
      'input_max': 1,
      'cm_id': CM_ID
      }
 
 ]
-INPUTS_CALCULATION_MODULE2 = [
 
-    {'input_name': 'Min. heat demand in hectare',
-     'input_type': 'input',
-     'input_parameter_name': 'pix_threshold',
-     'input_value': 333,
-     'input_unit': 'MWh/ha',
-     'input_min': 0,
-     'input_max': 1000,
-     'cm_id': CM_ID
-     },
-    {'input_name': 'Min. heat demand in a DH area',
-     'input_type': 'input',
-     'input_parameter_name': 'DH_threshold',
-     'input_value': 30,
-     'input_unit': 'GWh/year',
-     'input_min': 0,
-     'input_max': 500,
-     'cm_id': CM_ID
-     }
-]
-
-SIGNATURE2 = {
-    "category": "Buildings",
-    "cm_name": CM_NAME,
-    "layers_needed": [
-        "heat_tot_curr_density_tif",
-    ],
-    "type_layer_needed": [
-        "heat",
-    ],
-    "cm_url": "Do not add something",
-    "cm_description": "CM generating new load profiles",
-    "cm_id": CM_ID,
-    'inputs_calculation_module': INPUTS_CALCULATION_MODULE
-}
 SIGNATURE = {
     "category": "Buildings",
     "cm_name": CM_NAME,
-    "layers_needed": [
-        "heat_tot_curr_density_tif",
-    ],
-    "type_layer_needed": [
-        "heat",
-    ],
+
     "cm_url": "Do not add something",
-    "cm_description": "This computation module calculates district heating " \
-                      "potential within the selected region. As output, a layer for the " \
-                      "potential areas are shown. Click on the regions to get their " \
-                      "corresponding potential. Within the indicator/graph window, relevant " \
-                      "indicators and charts regarding DH potential within the selected zone" \
-                      "and potentials in sub-zones are illustrated.",
+    "cm_description": "CM generating new load profiles",
     "cm_id": CM_ID,
     'inputs_calculation_module': INPUTS_CALCULATION_MODULE
 }
